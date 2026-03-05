0f1129a3478644fe9c3add848b1cfb6f3a9f50aa
Brian <mochibrian10@gmail.com>
2026-03-05 23:27:51 +0300

    suppress display errors for AJAX responses in contact form handler

--- forms/contact.php
+++ forms/contact.php
@@ -11,7 +11,7 @@
 
 // Configuration
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);  // Suppress display for AJAX responses
 
 // Include SMTP functions and config
 require_once 'smtp_mail.php';
